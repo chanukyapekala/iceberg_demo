759ac74a39c2f7e353f4bf1e00011e050f04c0bd
Chanukya Pekala <chanukya.pekala@gmail.com>
2025-04-27 17:22:17 +0300

    Added demo pdf

--- spark-shell.sh
+++ spark-shell.sh
@@ -1,5 +1,5 @@
 pyspark \
-  --packages org.apache.iceberg:iceberg-spark-runtime-3.4_2.12:1.5.0 \
+  --packages org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.5.0 \
   --conf spark.sql.catalog.local=org.apache.iceberg.spark.SparkCatalog \
   --conf spark.sql.catalog.local.type=hadoop \
   --conf spark.sql.catalog.local.warehouse=file:///Users/chanukya/GIT/iceberg_warehouse
